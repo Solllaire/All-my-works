70e75b60f0f3d98a6a0d4686f7f03f71d8e67243
Solllaire <113596697+Solllaire@users.noreply.github.com>
2022-11-11 17:25:39 +0300

    1 \320\272\321\203\321\200\321\201/#	new file:   "2_\320\272\321\203\321\200\321\201/Git/\320\240\320\260\320\261\320\276\321\202\320\260 \321\201 \320\262\320\265\321\202\320\272\320\260\320\274\320\270/\320\220\320\261\321\200\320\260\320\274\320\276\320\262_\320\234\320\260\321\201\320\273\320\276\320\262.docx" Работы

---
+++ Своё/CalculatorWFR/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button16_Click_1(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if ((textBox1.Text.IndexOf(",") > -1 && b.Text == ",") | (textBox1.Text == "0" && b.Text!=",") | (b.Text == ","&& textBox1.Text==""))
+            {
+                return;
+            }
+            else
+            textBox1.Text = textBox1.Text + b.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            }
+            catch
+            {
+                return;
+            }
+        }
+        String action, number1;
+
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                Button b = (Button)sender;
+                if (b.Text == "/" | b.Text == "*") return;
+                else
+                textBox1.Text = "";
+                textBox1.Text = textBox1.Text + b.Text;
+            }
+            else
+            {
+                Button b = (Button)sender;
+                if (textBox1.Text == "+" | textBox1.Text == "-") { return; }
+                action = b.Text;
+                number1 = textBox1.Text;
+                textBox1.Text = "";
+            }
+            textBox2.Text = number1+" "+action;
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (action == "+")
+                    textBox1.Text = (Convert.ToDouble(textBox1.Text) + Convert.ToDouble(number1)).ToString();
+                if (action == "-")
+                    textBox1.Text = (Convert.ToDouble(number1) - Convert.ToDouble(textBox1.Text)).ToString();
+                if (action == "*")
+                    textBox1.Text = (Convert.ToDouble(textBox1.Text) * Convert.ToDouble(number1)).ToString();
+                if (action == "/")
+                    textBox1.Text = (Convert.ToDouble(number1) / Convert.ToDouble(textBox1.Text)).ToString();
+                action = "";
+                textBox2.Text = "";
+
+
+            }
+            catch { textBox2.Text = ""; return;}
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
